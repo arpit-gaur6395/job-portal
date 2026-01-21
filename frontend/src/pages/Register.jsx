3a1cf8318773122db746c9de551321891a103dae
Arpit Gaur <gaur68124@gmail.com>
2026-01-21 12:36:06 +0530

    Fix import path for AuthContext in Register component

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,7 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/authContext";
 import "../styles/Register.css"; 
 
 function Register() {
@@ -77,3 +77,4 @@ function Register() {
 }
 
 export default Register;
+
